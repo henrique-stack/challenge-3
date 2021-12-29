3c2336c70697dce7650fc12c3572877826a01165
Henrique <henriquelimaribeiro4@gmail.com>
2021-12-29 20:06:23 -0300

    Hello World!

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from 'typeorm';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+  async findUserWithGamesById({
+    user_id
+    }: IFindUserWithGamesDTO): Promise<User| undefined> { 
+        const userWithGames = this.repository.findOne(user_id, { relations: ['games']})
+        
+        if(!userWithGames) throw new Error('User not found')
+  
+        return userWithGames
+    };
+  
+    async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return  await this.repository.query('SELECT * FROM users ORDER BY first_name')
+    
+    }
+  
+    async findUserByFullName({
+      first_name,
+      last_name
+    }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+     return await this.repository.query(`SELECT * FROM users WHERE first_name ILIKE $1 AND last_name ILIKE $2`, [first_name, last_name])
+  }
+}
